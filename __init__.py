fa496236107c2d337e75f2e98b8b14e36fb025bf
Juan <administrator@Juans-MacBook-Air.local>
2020-11-15 19:30:34 -0500

    initial commit

---
+++ __init__.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect
+from werkzeug.utils import secure_filename
+import os
+import uuid
+
+app = Flask(__name__)
+
+@app.route('/')
+def step_1():
+    return render_template('step_1.html')
+
+@app.route('/step-2', methods=['GET', 'POST'])
+def step_2():
+    if request.method == 'GET':
+        return render_template('step_2.html')
+    else:
+        f = request.files['file']
+        f.save(os.getcwd() + '/temp/' + str(uuid.uuid4()))
+        return redirect("/step-3", code=302)
+
+@app.route('/step-3')
+def step_3():
+    return render_template('step_3.html')
+
+@app.route('/static/<path:path>')
+def send_js(path):
+    return send_from_directory('static', path)
